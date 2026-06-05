933bffe0ef095dd31f011a45eac08867be1eddeb
Leif Eric Fredheim <463422+leifericf@users.noreply.github.com>
2026-06-06 00:22:57 +0200

    jit: drop stale POSIX includes blocking the windows JIT build

--- src/eval/bc/jit/entry.c
+++ src/eval/bc/jit/entry.c
@@ -27,8 +27,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/mman.h>
-#include <unistd.h>
 
 #include "../../../prim/internal.h"
 #include "../stencils/runtime_layout.h"
