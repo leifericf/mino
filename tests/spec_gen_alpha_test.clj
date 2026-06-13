32d10ab6f9d0155610e830209e8e2826112fe303
Leif Eric Fredheim <463422+leifericf@users.noreply.github.com>
2026-06-13 10:06:55 +0200

    Tests: Register the seven new bundled-namespace test files

--- tests/spec_gen_alpha_test.clj
+++ tests/spec_gen_alpha_test.clj
@@ -1,5 +1,5 @@
 (require "tests/test")
-(require '[clojure.spec.gen.alpha :as gen])
+(require '[clojure.spec.gen.alpha :as sgen])
 
 ;; clojure.spec.gen.alpha is the generator wrapper spec uses to build
 ;; sample data; it delegates to the bundled test.check generators. The
@@ -7,73 +7,73 @@
 ;; and assert membership or shape for the random ones.
 
 (deftest return-yields-its-value
-  (is (= 5 (gen/generate (gen/return 5))))
-  (is (= :x (gen/generate (gen/return :x)))))
+  (is (= 5 (sgen/generate (sgen/return 5))))
+  (is (= :x (sgen/generate (sgen/return :x)))))
 
 (deftest sample-count-and-membership
-  (let [vs (gen/sample (gen/elements [1 2 3]) 10)]
+  (let [vs (sgen/sample (sgen/elements [1 2 3]) 10)]
     (is (= 10 (count vs)))
     (is (every? #{1 2 3} vs))))
 
 (deftest fmap-transforms-values
-  (is (= 2 (gen/generate (gen/fmap inc (gen/return 1)))))
+  (is (= 2 (sgen/generate (sgen/fmap inc (sgen/return 1)))))
   (is (every? even?
-              (gen/sample (gen/fmap #(* 2 %) (gen/elements [1 2 3])) 20))))
+              (sgen/sample (sgen/fmap #(* 2 %) (sgen/elements [1 2 3])) 20))))
 
 (deftest tuple-pairs-positions
-  (let [vs (gen/sample (gen/tuple (gen/return :a) (gen/return :b)))]
+  (let [vs (sgen/sample (sgen/tuple (sgen/return :a) (sgen/return :b)))]
     (is (every? #(= [:a :b] %) vs))))
 
 (deftest such-that-constrains-values
-  (let [vs (gen/sample (gen/such-that even? (gen/elements [2 4 6 8])) 15)]
+  (let [vs (sgen/sample (sgen/such-that even? (sgen/elements [2 4 6 8])) 15)]
     (is (every? even? vs))))
 
 (deftest one-of-draws-from-its-generators
-  (let [vs (gen/sample (gen/one-of [(gen/return :a) (gen/return :b)]) 20)]
+  (let [vs (sgen/sample (sgen/one-of [(sgen/return :a) (sgen/return :b)]) 20)]
     (is (every? #{:a :b} vs))))
 
 (deftest vector-produces-vectors
-  (let [vs (gen/sample (gen/vector (gen/return 0) 3) 5)]
+  (let [vs (sgen/sample (sgen/vector (sgen/return 0) 3) 5)]
     (is (every? vector? vs))
     (is (every? #(= [0 0 0] %) vs))))
 
 (deftest choose-stays-in-range
-  (let [vs (gen/sample (gen/choose 5 10) 30)]
+  (let [vs (sgen/sample (sgen/choose 5 10) 30)]
     (is (every? #(and (<= 5 %) (<= % 10)) vs))
     (is (every? integer? vs))))
 
 (deftest hash-map-fixed-keys
-  (let [vs (gen/sample (gen/hash-map :a (gen/return 1) :b (gen/return 2)) 5)]
+  (let [vs (sgen/sample (sgen/hash-map :a (sgen/return 1) :b (sgen/return 2)) 5)]
     (is (every? #(= {:a 1 :b 2} %) vs))))
 
 (deftest bind-chains-generators
-  (let [g (gen/bind (gen/return 3)
-                    (fn [n] (gen/return (* n n))))]
-    (is (= 9 (gen/generate g)))))
+  (let [g (sgen/bind (sgen/return 3)
+                    (fn [n] (sgen/return (* n n))))]
+    (is (= 9 (sgen/generate g)))))
 
 (deftest frequency-picks-from-pairs
-  (let [vs (gen/sample
-             (gen/frequency [[1 (gen/return :rare)] [3 (gen/return :common)]])
+  (let [vs (sgen/sample
+             (sgen/frequency [[1 (sgen/return :rare)] [3 (sgen/return :common)]])
              40)]
     (is (every? #{:rare :common} vs))))
 
 (deftest large-integer-yields-integers
-  (is (every? integer? (gen/sample (gen/large-integer* {:min 0 :max 9}) 20)))
+  (is (every? integer? (sgen/sample (sgen/large-integer* {:min 0 :max 9}) 20)))
   (is (every? #(and (<= 0 %) (<= % 9))
-              (gen/sample (gen/large-integer* {:min 0 :max 9}) 20))))
+              (sgen/sample (sgen/large-integer* {:min 0 :max 9}) 20))))
 
 (deftest scalar-generators-have-expected-shape
-  (is (every? boolean? (gen/sample gen/boolean 10)))
-  (is (every? integer? (gen/sample gen/int 10)))
-  (is (every? string? (gen/sample gen/string 10)))
-  (is (every? keyword? (gen/sample gen/keyword 10)))
-  (is (every? symbol? (gen/sample gen/symbol 10)))
-  (is (every? uuid? (gen/sample gen/uuid 5))))
+  (is (every? boolean? (sgen/sample sgen/boolean 10)))
+  (is (every? integer? (sgen/sample sgen/int 10)))
+  (is (every? string? (sgen/sample sgen/string 10)))
+  (is (every? keyword? (sgen/sample sgen/keyword 10)))
+  (is (every? symbol? (sgen/sample sgen/symbol 10)))
+  (is (every? uuid? (sgen/sample sgen/uuid 5))))
 
 (deftest qualified-name-generators
   (is (every? #(and (keyword? %) (namespace %))
-              (gen/sample gen/keyword-ns 10)))
+              (sgen/sample sgen/keyword-ns 10)))
   (is (every? #(and (symbol? %) (namespace %))
-              (gen/sample gen/symbol-ns 10))))
+              (sgen/sample sgen/symbol-ns 10))))
 
 (run-tests-and-exit)
