b36906b95444cdb5f7eff305902a2edb5eef4c8b
Leif Eric Fredheim <463422+leifericf@users.noreply.github.com>
2026-06-27 17:11:15 +0200

    Fix: Compute CRC32 table at runtime and verify on load

--- src/runtime/image.c
+++ src/runtime/image.c
@@ -37,56 +37,34 @@
 #define IMG_HT_INIT     256
 #define IMG_HT_LOAD     75
 
-/* --- CRC32 (IEEE 802.3 polynomial, table-less) -------------------- */
+/* --- CRC32 (IEEE 802.3 polynomial 0xEDB88320) -------------------- */
+
+/* Table computed once on first use. Avoids the transcription-error risk
+ * of a hand-typed 256-entry constant table. */
+static uint32_t img_crc32_table[256];
+static int img_crc32_table_ready = 0;
+
+static void img_crc32_table_init(void)
+{
+    uint32_t i;
+    for (i = 0; i < 256; i++) {
+        uint32_t c = i;
+        int j;
+        for (j = 0; j < 8; j++)
+            c = (c & 1) ? (0xEDB88320u ^ (c >> 1)) : (c >> 1);
+        img_crc32_table[i] = c;
+    }
+    img_crc32_table_ready = 1;
+}
 
 static uint32_t img_crc32_update(uint32_t crc, const void *data, size_t len)
 {
-    static const uint32_t ktable[256] = {
-        0x00000000,0x77073096,0xEE0E612C,0x990951BA,0x076DC419,0x706AF48F,
-        0xE963A535,0x9E6495A3,0x0EDB8832,0x79DCB8A4,0xE0D5E91E,0x97D2D988,
-        0x09B64C2B,0x7EB17CBD,0xE7B82D07,0x90BF1D91,0x1DB71064,0x6AB020F2,
-        0xF3B97148,0x84BE41DE,0x1ADAD47D,0x6DDDE4EB,0xF4D4B551,0x83D385C7,
-        0x136C9856,0x646BA8C0,0xFD62F97A,0x8A65C9EC,0x14015C4F,0x63066CD9,
-        0xFA0F3D63,0x8D080DF5,0x3B6E20C8,0x4C69105E,0xD56041E4,0xA2677172,
-        0x3C03E4D1,0x4B04D447,0xD20D85FD,0xA50AB56B,0x35B5A8FA,0x42B2986C,
-        0xDBBBC9D6,0xACBCF940,0x32D86CE3,0x45DF5C75,0xDCD60DCF,0xABD13D59,
-        0x26D930AC,0x51DE003A,0xC8D75180,0xBFD06116,0x21B4F4B5,0x56B3C423,
-        0xCFBA9599,0xB8BDA50F,0x2802B89E,0x5F058808,0xC60CD9B2,0xB10BE924,
-        0x2F6F7C87,0x58684C11,0xC1611DAB,0xB6662D3D,0x76DC4190,0x01DB7106,
-        0x98D220BC,0xEFD5102A,0x71B18589,0x06B6B51F,0x9FBFE4A5,0xE8B8D433,
-        0x7807C9A2,0x0F00F934,0x9609A88E,0xE10E9818,0x7F6A0D0B,0x086D3D2D,
-        0x91646C97,0xE6635C01,0x6B6B51F4,0x1C6C6162,0x856530D8,0xF262004E,
-        0x6C0695ED,0x1B01A57B,0x8208F4C1,0xF50FC457,0x65B0D9C6,0x12B7E950,
-        0x8BBEB8EA,0xFCB9887C,0x62DD1DDF,0x15DA2D49,0x8CD37CF3,0xFBD44C65,
-        0x4DB26158,0x3AB551CE,0xA3BC0074,0xD4BB30E2,0x4ADFA541,0x3DD895D7,
-        0xA4D1C46D,0xD3D6F4FB,0x4369E96A,0x346ED9FC,0xAD678846,0xDA60B8D0,
-        0x44042D73,0x33031DE5,0xAA0A4C5F,0xDD0D7ACC,0x5005713C,0x270241AA,
-        0xBE0B1010,0xC90C2086,0x5768B525,0x206F85B3,0xB966D409,0xCE61E49F,
-        0x5EDEF90E,0x29D9C998,0xB0D09822,0xC7D7A8B4,0x59B33D17,0x2EB40D81,
-        0xB7BD5C3B,0xC0BA6CAD,0xEDB88320,0x9ABFB3B6,0x03B6E20C,0x74B1D29A,
-        0xEAD54739,0x9DD277AF,0x04DB2615,0x73DC1683,0xE3630B12,0x94643B84,
-        0x0D6D6A3E,0x7A6A5AA8,0xE40ECF0B,0x9309FF9D,0x0A00AE27,0x7D079EB1,
-        0xF00F9344,0x8708A3D2,0x1E01F268,0x6906C2FE,0xF762575D,0x806567CB,
-        0x196C3671,0x6E6B06E7,0xFED41B76,0x89D32BE0,0x10DA7A5A,0x67DD4ACC,
-        0xF9B9DF6F,0x8EBEEFF9,0x17B7BE43,0x60B08ED5,0xD6D6A3E8,0xA1D1937E,
-        0x38D8C2C4,0x4FDFF252,0xD1BB67F1,0xA6BC5767,0x3FB506DD,0x48B2364B,
-        0xD80D2BDA,0xAF0A1B4C,0x36034AF6,0x41047A60,0xDF60EFC3,0xA8670855,
-        0x316048D1,0x4669BE79,0xCB61B38C,0xBC66831A,0x256FD2A0,0x5268E236,
-        0xCC0C7795,0xBB0B4703,0x220216B9,0x5505262F,0xC5BA3BBE,0xB2BD0B28,
-        0x2BB45A92,0x5CB36A04,0xC2D7FFA7,0xB5D0CF31,0x2CD99E8B,0x5BDEAE1D,
-        0x9B64C2B0,0xEC63F226,0x756AA39C,0x026D930A,0x9C0906A9,0xEB0E363F,
-        0x72076785,0x05005713,0x95BF4A82,0xE2B87A14,0x7BB12BAE,0x0CB61B38,
-        0x92D28E9B,0xE5D5BE0D,0x7CDCEFB7,0x0BDBDF21,0x86D3D2D4,0xF1D4E242,
-        0xBD062F0F,0x10562950,0x8AB6FE6B,0xC4BC7998,0xD1A5E2F8,0xFE4350C7,
-        0xA34F6E20,0xDF9D0E5F,0xA34F5371,0xD00D5B3B,0x998F3648,0x6F61F4DE,
-        0x87764B0E,0x209D3639,0x4ED5A9D4,0x7F6A4D29,0x87764B0E,0x209D3639,
-        0x4ED5A9D4,0x7F6A4D29
-    };
     const unsigned char *p = (const unsigned char *)data;
     size_t i;
+    if (!img_crc32_table_ready) img_crc32_table_init();
     crc = ~crc;
     for (i = 0; i < len; i++)
-        crc = ktable[(crc ^ p[i]) & 0xFF] ^ (crc >> 8);
+        crc = img_crc32_table[(crc ^ p[i]) & 0xFF] ^ (crc >> 8);
     return ~crc;
 }
 
@@ -762,7 +740,8 @@ int mino_save_image(mino_state *S, const char *path)
     idt.core_env = S->ns_vars.mino_core_env;
     img_walk_roots(S, &idt);
 
-    f = fopen(path, "wb");
+    /* Open read-write so we can read the file back for CRC verification */
+    f = fopen(path, "wb+");
     if (f == NULL) {
         img_idt_free(&idt);
         set_eval_diag(S, NULL, "io", "MIO001",
@@ -1377,6 +1356,36 @@ int mino_load_image_into(mino_state *S, const char *path)
                        "load-image: bad magic");
         free(buf);
         return -1;
+    }
+
+    /* Verify CRC32: find the trailer, compute CRC over everything before it */
+    {
+        char       *crc_pos = NULL;
+        char       *search;
+        uint32_t    stored_crc = 0;
+        uint32_t    computed_crc;
+        /* Find the last "CRC32 " line */
+        for (search = buf; (search = strstr(search, "\nCRC32 ")) != NULL; )
+            crc_pos = ++search;
+        if (crc_pos != NULL) {
+            /* crc_pos points right after the \n before CRC32 */
+            stored_crc = (uint32_t)strtoul(crc_pos + 5, NULL, 16);
+            /* Compute CRC over everything from start to the \n before CRC32 */
+            *crc_pos = '\0';  /* truncate at the \n */
+            computed_crc = img_crc32_update(0, buf, (size_t)(crc_pos - buf));
+            if (computed_crc != stored_crc) {
+                char diag[128];
+                snprintf(diag, sizeof diag,
+                    "load-image: CRC mismatch (stored %08x, computed %08x)",
+                    stored_crc, computed_crc);
+                set_eval_diag(S, NULL, "io", "MIO001", diag);
+                free(buf);
+                return -1;
+            }
+            /* Restore the \n for line parsing */
+            *crc_pos = '\n';
+        }
+        /* If no CRC32 line found, proceed without verification (v0 compat) */
     }
 
     img_reader_init(&r, S, ns_env_ensure(S, "clojure.core"));
